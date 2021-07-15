3fc999611e0055ec89198985fb951f09d367c2ff
schemak@pulipinc.com <schemak@pulipinc.com>
2021-07-15 14:16:50 +0900

    GTM적용

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import VueGtag from 'vue-gtag';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import store from './store';
@@ -6,4 +7,9 @@ import store from './store';
 createApp(App)
   .use(store)
   .use(vuetify)
+  .use(VueGtag, {
+    property: {
+      id: 'GTM-58VTNG5',
+    },
+  })
   .mount('#app');
