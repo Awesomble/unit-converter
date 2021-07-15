2b424a63bb2a73da286b260f9694c248e699d1dc
schemak@pulipinc.com <schemak@pulipinc.com>
2021-07-15 15:00:28 +0900

    추적코드변경

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import VueGtag from 'vue-gtag';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import store from './store';
@@ -7,9 +6,4 @@ import store from './store';
 createApp(App)
   .use(store)
   .use(vuetify)
-  .use(VueGtag, {
-    property: {
-      id: 'G-6VWR11TPSP',
-    },
-  })
   .mount('#app');
