fc56adc07cdbf2940fb3fe153333a73ff3604a41
schemak@pulipinc.com <schemak@pulipinc.com>
2021-07-19 08:20:47 +0900

    추적코드변경

--- src/App.vue
+++ src/App.vue
@@ -266,6 +266,11 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
 .money {
   &:before {
     content: '🎉';
